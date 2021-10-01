c7dd58c52822eb6724e9645bf94d6ceadf8fa369
Megha <tinavin32@gmail.com>
2021-10-01 14:27:59 +0530

    Initial commit

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Hello World";
+            string second = new object<string>("Hello World");
+
+            bool equal1 = first == second;
+            bool equal2 = first.Equals(second);
+
+            Console.WriteLine("== is : {0}", equal1);
+            Console.WriteLine("Equals is : {0}", equal2);
+            Console.ReadKey();
+        }
+    }
+}
